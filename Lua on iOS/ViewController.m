8a440fcf150bae1b7bb713659aa0cc43f9ecbbbb
Maciej Konieczny <hello@narf.pl>
2013-04-07 01:30:43 +0200

    tweaked style

--- Lua on iOS/ViewController.m
+++ Lua on iOS/ViewController.m
@@ -8,22 +8,17 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
 
 @end
 
+
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
